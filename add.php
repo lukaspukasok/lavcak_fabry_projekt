8b03b696a26f8a19359f83f6b6608a5e79160ca1
Daniel Fábry <dfabry116@gmail.com>
2026-04-20 08:28:12 +0200

    pridanie add

---
+++ add.php
@@ -0,0 +1,37 @@
+<?php
+require_once("../config/db.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $title = $_POST["title"];
+
+    if (!empty($title)) {
+        $sql = "INSERT INTO tasks (title) VALUES ('$title')";
+        $conn->query($sql);
+
+        header("Location: index.php");
+        exit();
+    } else {
+        echo "Zadaj názov úlohy!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pridať úlohu</title>
+</head>
+<body>
+
+<h1>Pridať úlohu</h1>
+
+<form method="POST">
+    <input type="text" name="title" placeholder="Názov úlohy">
+    <button type="submit">Pridať</button>
+</form>
+
+<br>
+<a href="index.php">Späť</a>
+
+</body>
+</html>
