fa0664974c2647a85689b391f3a912c55754b4ef
Daniel Fábry <dfabry116@gmail.com>
2026-04-30 10:09:54 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,13 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$dbHost = "localhost";
-$dbUser = "root";
-$dbPass = "root";
-$dbName = "todo_app";
-$dbPort = 3306;
+$conn = mysqli_connect("localhost","root","root","todo_app", 3306);
 
-$conn = @mysqli_connect($dbHost, $dbUser, $dbPass, $dbName, $dbPort);
 if (!$conn) {
     die(
         "Nepodarilo sa pripojit k databaze '$dbName'. " .
